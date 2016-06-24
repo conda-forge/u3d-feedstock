7b533c7e364e0d5153569a718cd07b97cc010c0c
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2016-06-24 16:21:45 +0200

    Disabled shared build on Windows, not supported according to README

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,7 @@ cmake .. -G "NMake Makefiles" ^
     -Wno-dev ^
     -DCMAKE_BUILD_TYPE=%BUILD_CONFIG% ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-    -DU3D_SHARED:BOOL=TRUE
+    -DU3D_SHARED:BOOL=FALSE
 if errorlevel 1 exit 1
 
 nmake install
