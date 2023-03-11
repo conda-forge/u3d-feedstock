5597c29b32c87684078214a55a7e989e236df280
Mark Harfouche <mark.harfouche@gmail.com>
2023-03-11 09:24:42 -0500

    CMAKE_CXX_STANDARD 14

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cd build
 cmake .. -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=$BUILD_CONFIG \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCXX_STANDARD=14 \
+    -DCMAKE_CXX_STANDARD=14 \
     -DU3D_SHARED:BOOL=ON
 
 make install
